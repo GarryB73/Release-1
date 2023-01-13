ce5c4aa344482d1fb6d148a668c0fb95062d883d
GarryB73 <122373091+GarryB73@users.noreply.github.com>
2023-01-13 14:10:34 +0000

    common added winrm updated

--- Windows/Enable_Winrm.ps1
+++ Windows/Enable_Winrm.ps1
@@ -1,9 +1,17 @@
-# Configure Firewall to allow ICMP traffic
-
-
-
-
 # Configure WinRM to allow unsecure connection
 
 Set-Item -Path WSMan:\localhost\Service\AllowUnencrypted -Value true
 Set-Item -Path WSMan:\localhost\Service\Auth\Basic -Value true
+
+# Configure Firewall to allow ICMP traffic
+#- name: Firewall | Allow RDP through Firewall
+#  win_dsc:
+#    resource_name: xFirewall
+#    Name: "Administrator access for RDP (TCP-In)"
+#    Ensure: present
+#    Enabled: True
+#    Profile: "Domain"
+#    Direction: "Inbound"
+#    Localport: "3389"
+#    Protocol: "TCP"
+#    Description: "Opens the listener port for RDP"
