16188d70a5c1c54c4cab7561d00ce6e56ef4f65a
GarryB73 <122373091+GarryB73@users.noreply.github.com>
2023-01-17 13:01:47 +0000

    updates

--- Windows/Enable_Winrm.ps1
+++ Windows/Enable_Winrm.ps1
@@ -3,6 +3,9 @@
 Set-Item -Path WSMan:\localhost\Service\AllowUnencrypted -Value true
 Set-Item -Path WSMan:\localhost\Service\Auth\Basic -Value true
 
+# Install Chocolatey software
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
 # Configure Firewall to allow ICMP traffic
 #- name: Firewall | Allow RDP through Firewall
 #  win_dsc:
