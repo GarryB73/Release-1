a2c87795a150f4f8fd78d53e64955ccb7ee3d249
GarryB73 <122373091+GarryB73@users.noreply.github.com>
2023-01-30 14:56:43 +0000

    updates

---
+++ SQL/SQL-Add-Group-Permissions.ps1
@@ -0,0 +1,20 @@
+# Set instance & credentials to login into SQL Server
+$Server = "TESTSQL001"
+$Domain = "Test"
+$Credential = "sa"
+$Database = "SharePoint"
+
+# Add SysAdmin group to SQL Users
+Add-SqlLogin -ServerInstance $Server -Credential $Credential -LoginName $Domain"\SysAdmin" -LoginType "WindowsGroup" -DefaultDatabase $Database
+
+# Add SecurityAdmin group to SQL Users
+Add-SqlLogin -ServerInstance $Server -Credential $Credential -LoginName $Domain"\SecurityAdmin" -Login "WindowsGroup" -DefaultDatabase $Database
+
+# Add DB_Creator group to SQL Users
+Add-SqlLogin -ServerInstance $Server -Credential $Credential -LoginName $Domain"\DB_Creator" -LoginType "WindowsGroup" -DefaultDatabase $Database
+
+# Assign Permissions to SysAdmin
+
+# Assign Permissions to SecurityAdmin
+
+# Assign Permissions to DB_Creator
